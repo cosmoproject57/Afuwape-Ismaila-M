fa76614b49676ed7573a4c0f091676d12edca4e6
Cloud User <ec2-user@ip-172-31-94-84.ec2.internal>
2021-11-23 02:52:07 +0000

    calculator script

---
+++ calculator.sh
@@ -0,0 +1,11 @@
+
+echo " Please Enter the first num "
+read num1
+echo " Please Enter the 2nd num "  
+read num2 
+
+echo The sum is   `expr $num1 + $num2` 
+echo The difference is   `expr $num1 - $num2` 
+echo The product is `expr  $num1 \* $num2`
+echo The division is `expr  $num1 / $num2` 
+echo The sum is `expr $num1 - $num2`
