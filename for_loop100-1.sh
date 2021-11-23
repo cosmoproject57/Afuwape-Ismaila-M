18610d9b5b02c6a96515942f1518c4876525435a
Cloud User <ec2-user@ip-172-31-94-84.ec2.internal>
2021-11-23 02:13:47 +0000

    for loop to count 100-1 and switch case to take input 1-5 from user

---
+++ for_loop100-1.sh
@@ -0,0 +1,8 @@
+
+#A shell to print numbers form 100-1 using for-loop
+#!/bin/sh
+for (( a=100; a>=1; a--))
+do
+ echo $a
+done
+
---
+++ switch_case_value1-5.sh
@@ -0,0 +1,20 @@
+#A switch case to take value from 1 to 5
+#!/bin/bash
+echo 'Enter value from 1 to 5' 
+read number
+case $number in 
+  1) 
+echo "user enter value $number sucessfully" ;;
+  2)
+echo "user enter value $number sucessfully" ;;
+  3)
+echo "user enter value $number sucessfully" ;;
+  4)
+echo "user enter value $number sucessfully" ;;
+  5)
+echo "user enter value $number sucessfully" ;;
+  *) 
+echo 'sorry you passed the wrong value option'
+echo 'Value options are 1|2|3|4|5' ;;
+esac
+echo 'switch case ends'
