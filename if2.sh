9682f3cc87ae186209630ea7ebd9ecc61b2b6bd0
Cloud User <ec2-user@ip-172-31-94-84.ec2.internal>
2021-11-23 03:00:46 +0000

    if statement to read pin number

---
+++ if2.sh
@@ -0,0 +1,16 @@
+echo Please enter your name 
+read name
+echo welcome $name
+
+mypin=1988
+echo Please enter your pin
+read pin
+if [ $pin == $mypin ]
+then 
+	echo 'The ping you entered is correct'
+else
+	echo "sorry" $name "You entered a wrong pin"
+
+
+fi
+
